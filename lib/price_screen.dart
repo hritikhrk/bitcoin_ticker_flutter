a690f69dce68f7ca303359e12a08f1b47d0c345b
Hritik Kumar <hritik.kmr.111@gmail.com>
2021-10-06 17:50:01 +0530

    refactor code

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -2,6 +2,7 @@ import 'package:bitcoin_ticker_flutter/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
+import 'package:bitcoin_ticker_flutter/crypto_card.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -111,43 +112,6 @@ class _PriceScreenState extends State<PriceScreen> {
             child: Platform.isIOS ? iOSPicker() : getDropdownButton(),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class CryptoCard extends StatelessWidget {
-  CryptoCard({
-    this.value,
-    this.selectedCurrency,
-    this.cryptoCurrency,
-  });
-
-  final String value;
-  final String selectedCurrency;
-  final String cryptoCurrency;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-      child: Card(
-        color: Colors.lightBlueAccent,
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-          child: Text(
-            '1 $cryptoCurrency = $value $selectedCurrency',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.white,
-            ),
-          ),
-        ),
       ),
     );
   }
